745572ce39cf54d364d87089727934cee2bd6006
Gabriel Honda <vegancraft13@gmail.com>
2020-06-07 11:36:42 -0400

    Testes

--- src/test/java/com/example/carros/CarrosApplicationTests.java
+++ src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CarrosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void insertTest() {}
 
+    @Test
+    public void deleteTest() {
+
+    }
 }
